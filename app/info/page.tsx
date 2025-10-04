e50baeeb14a86f3c2b4ceb22acad0f234c788faf
xXKeNdAmAmAsTeRXx <frkubala@student.agh.edu.pl>
2025-10-05 01:52:36 +0200

    add invisible info button

---
+++ app/info/page.tsx
@@ -0,0 +1,15 @@
+import type { Metadata } from "next";
+import { Press_Start_2P } from "next/font/google";
+import "./globals.css";
+import "./styles/info.css"
+
+
+
+export default function InfoPage() {
+    return (
+        <div>
+            <h1>Information Page</h1>
+            <p>This is the information page content.</p>
+        </div>
+    );
+}
