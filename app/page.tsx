27d0255d3f0896d5ce7ae1dbf24951d956ea77e4
xXKeNdAmAmAsTeRXx <frkubala@student.agh.edu.pl>
2025-10-05 01:41:12 +0200

    Menu Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Auth from "@/app/hooks/Auth";
-
+import { MenuButton } from "./components/Button";
+import { SwipeGoButton } from "./components/Button";
 export default function Home() {
     return (
         <Auth>
             <div className="w-full h-full">
+            <div className="menu-container">
+                
+                <SwipeGoButton swipe="SWIPE" and="&" Go="GO"/>
+                <MenuButton text="Saved"/>
+                <MenuButton text="Plan"/>
+                <img  className="info-icon" src="./icons/infoYellow.png" alt="Info" />
+                
+            </div>
+            
 
             </div>
         </Auth>
